f94832917fe1c8fb51229011d632c6452f12e161
Daiane Cruz <daianercruz12@gmail.com>
2024-11-07 11:55:39 -0300

    feat: realizando mais consultas

--- manipulando_dados_excel.py
+++ manipulando_dados_excel.py
@@ -43,8 +43,6 @@ df["Receita"]= df["Vendas"].mul(df["Qtde"])
 print(df.head())
 
 
-
-
 print(f" Retornando a maior receita: {df["Receita"].max()}")
 
 
@@ -54,3 +52,40 @@ print(f" Retornando a menor receita:{df["Receita"].min()}")
 df.groupby("Cidade")["Receita"].sum()
 
 print(df.sort_values("Receita", ascending=False).head(15))
+
+
+#Trabalhando com datas 
+
+df["Data"] = df["Data"].astype("int64")
+print(df.dtypes)
+
+
+df["Data"] = pd.to_datetime(df["Data"])
+print(df.dtypes)
+
+# Agrupamento por ano
+df.groupby(df["Data"].dt.year)["Receita"].sum()
+
+# Criando uma nova coluna com o ano
+df["Ano_Venda"] = df["Data"].dt.year
+
+print(df.sample(5))
+
+
+# Retornando a data mais antiga
+print(f"Retornando a data mais antiga:{df["Data"].min()}")
+
+# Calculando a diferença de dias
+df["diferenca_dias"] = df["Data"] - df["Data"].min()
+print(df.sample(5))
+
+# Criando a coluna de trimestre
+df["trimestre_vendas"] = df["Data"].dt.quarter
+print(df.sample(5))
+
+# Filtrando as vendas de 2019 do mês de março
+vendas_marco_19 = df.loc[(df["Data"].dt.year == 2019) &(df["Data"].dt.month == 3)]
+     
+
+print(vendas_marco_19)
+
